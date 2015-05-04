1984382d6c4f1cbd1fafa4e4a2eea3252164a6c4
test19877 <testingnestingpesting@gmail.com>
2015-05-04 12:18:40 +0200

    SQL code examples

---
+++ SQLCodes/ReplicationAddingNewArticles.sql
@@ -0,0 +1,24 @@
+-- USE [publisherdatabase]
+--USE [DatawarehouseOds]
+GO
+
+-- checking fields allow_anonymous, immediate_sync, both should be false (0)
+-- for adding certain number of articles
+sp_helppublication
+
+
+--Run on your publisher database
+EXEC sp_changepublication 
+@publication = 'ODS', 
+@property = 'allow_anonymous' , 
+@value = 'false' 
+GO 
+
+EXEC sp_changepublication 
+@publication = 'ODS', 
+@property = 'immediate_sync' , 
+@value = 'false' 
+GO 
+
+-- after this add articles in wizard and start Snapshot agent
+
